be3b685a17792476e273aa47f1f887741358b00b
Yüce <yuceyuce99@gmail.com>
2020-11-22 17:48:45 +0300

    Implement fish hook pairs of a graph

--- class_precedence.py
+++ class_precedence.py
@@ -17,14 +17,36 @@ class Graph:
 
         self.adjacency_list[subclass].append(superclass)
 
+    def fish_hook_pairs(self):
+        result = {}
+        for key, values in self.items():
+            pairs = []
+            if len(values) == 0:
+                pairs.append((key, "Everything"))
+            else:
+                left = key
+                for value in values:
+                    right = value
+                    pairs.append((left, right))
+                    left = right
+            result[key] = pairs
+            result["Everything"] = [("Everything")]
+        return result.items()
 
 def main():
     obj = Graph()
-    obj.ako("BMW", "Car")
-    obj.ako("Car", "Vehicle")
-    obj.ako("BMW", "Fast")
-    for key, value in obj.items():
-        print(key, value)
+    obj.ako("Crazy", "Professors")
+    obj.ako("Crazy", "Hackers")
+    obj.ako("Professors", "Eccentrics")
+    obj.ako("Professors", "Teachers")
+    obj.ako("Hackers", "Eccentrics")
+    obj.ako("Hackers", "Programmers")
+    obj.ako("Eccentrics", "Dwarfs")
+    obj.ako("Teachers", "Dwarfs")
+    obj.ako("Programmers", "Dwarfs")
+    pairs = obj.fish_hook_pairs()
+    for pair, value in pairs:
+        print(pair, value)
 
 if __name__ == "__main__":
     main()
